1f74e0f21c9bced707a3e73e6b26164797af79a7
Mike Gillan <mike.gillan@gmail.com>
2012-06-18 10:17:20 -0400

    Updated streamripper scripts to save music into the HOME/Music folder

--- record_electro.sh
+++ record_electro.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-streamripper http://72.26.204.32:80/electrohouse_hi?a68c6e7d54e8c46 -u "Mozilla/5.0 (X11; Linux i686; rv:6.0) Gecko/20100101 Firefox/6.0" -t -r -R 0
+streamripper http://72.26.204.32:80/electrohouse_hi?a68c6e7d54e8c46 \
+-d $HOME/Music \
+-u "Mozilla/5.0 (X11; Linux i686; rv:6.0) Gecko/20100101 Firefox/6.0" \
+-t -r -R 0
