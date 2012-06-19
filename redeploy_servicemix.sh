84cdfdfa496b6dfa851c2226817501d95ae39ec6
Mike Gillan <mike.gillan@gmail.com>
2012-06-19 14:49:57 -0400

    Added features:addUrl and features:install lines to the redeploy_service.sh script in case we want to deploy the "old fashioned" way

--- redeploy_servicemix.sh
+++ redeploy_servicemix.sh
@@ -5,6 +5,14 @@ read -p "Press enter to redeploy ServiceMix (CTRL-C to abort)  "
 rm -rf apache-servicemix-4.4.1-fuse-03-06
 tar xvf apache-servicemix-4.4.1-fuse-03-06.configured.tar.gz -z
 
+echo
+echo "features:addUrl file:/n8id/elm/branches/3196/release/core_packager/src/main/resources/core-dependencies.xml"
+echo "features:addUrl file:/n8id/elm/branches/3196/release/dev_packager/src/main/resources/dev-dependencies.xml"
+echo "features:install elm-core-install"
+echo "features:install elm-dev-install"
+echo
+
 cd /usr/local/servicemix/bin
 ./servicemix
 
+exit 0
